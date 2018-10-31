871c8538815b09827668f0488e4c0faff7ba88a5
Romain Quéraud <r.queraud@catie.fr>
2018-10-31 09:00:35 +0100

    Add chart

--- chrome_plugin/src/Record.jsx
+++ chrome_plugin/src/Record.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { ButtonToolbar, Button, Row } from 'react-bootstrap';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 export default class Record extends React.Component {
 	constructor(props) {
@@ -10,20 +12,27 @@ export default class Record extends React.Component {
 			publish: true,
 			reinit: true,
 			redirect: false,
-			selector: ''
+			selector: '',
+			data: [100]
 		};
 		this.clickStart = this.clickStart.bind(this);
 		this.clickPublish = this.clickPublish.bind(this);
 		this.clickReinit = this.clickReinit.bind(this);
 		this.clickLogout = this.clickLogout.bind(this);
+
+		setInterval(() => {
+			let oldData = this.state.data;
+			oldData.push([Math.round(Math.random()*100)]);
+			this.setState({data: oldData});
+		}, 1500);
 	}
 
 	componentDidMount() {
-		chrome.runtime.sendMessage({kind:'getState'}, response => {
-			this.setState( () => {
+		chrome.runtime.sendMessage({ kind: 'getState' }, response => {
+			this.setState(() => {
 				return {
-					start: response.isRecording ,
-					publish: !response.isRecording ,
+					start: response.isRecording,
+					publish: !response.isRecording,
 					reinit: !response.isRecording,
 					redirect: false
 				};
@@ -33,12 +42,12 @@ export default class Record extends React.Component {
 
 	clickStart(event) {
 		event.preventDefault();
-		chrome.runtime.sendMessage({kind:'start'});
-		this.setState( () => {
+		chrome.runtime.sendMessage({ kind: 'start' });
+		this.setState(() => {
 			return {
-				start: true ,
-				publish: false ,
-				reinit: false ,
+				start: true,
+				publish: false,
+				reinit: false,
 				redirect: false
 			};
 		});
@@ -47,13 +56,13 @@ export default class Record extends React.Component {
 	clickPublish(event) {
 		event.preventDefault();
 		console.log('start publish');
-		chrome.runtime.sendMessage({kind:'publish'}, () => {
+		chrome.runtime.sendMessage({ kind: 'publish' }, () => {
 			console.log('publish ok');
-			this.setState( () => {
+			this.setState(() => {
 				return {
-					start: false ,
-					publish: true ,
-					reinit: true ,
+					start: false,
+					publish: true,
+					reinit: true,
 					redirect: false
 				};
 			});
@@ -62,12 +71,12 @@ export default class Record extends React.Component {
 
 	clickReinit(event) {
 		event.preventDefault();
-		chrome.runtime.sendMessage({kind:'reinit'});
-		this.setState( () => {
+		chrome.runtime.sendMessage({ kind: 'reinit' });
+		this.setState(() => {
 			return {
-				start: false ,
-				publish: true ,
-				reinit: true ,
+				start: false,
+				publish: true,
+				reinit: true,
 				redirect: false
 			};
 		});
@@ -75,22 +84,22 @@ export default class Record extends React.Component {
 
 	clickLogout(event) {
 		event.preventDefault();
-		chrome.runtime.sendMessage({kind:'logout'});
-		this.setState( () => {
+		chrome.runtime.sendMessage({ kind: 'logout' });
+		this.setState(() => {
 			return {
-				redirect : true
+				redirect: true
 			};
 		});
 	}
 
 	render() {
+		chrome.extension.getBackgroundPage().console.log('render');
 		if (this.state.redirect) {
-			return <Redirect to="/popup.html"/>;
+			return <Redirect to="/popup.html" />;
 		}
 		if (this.state.start) {
 			return (
 				<div>
-					<p>Share your groupSessionToken : {this.props.groupSessionToken}</p>
 					<ButtonToolbar>
 						<Button bsStyle="primary" onClick={this.clickPublish}>Publish</Button>
 						<Button bsStyle="danger" onClick={this.clickReinit}>Delete</Button>
@@ -100,9 +109,33 @@ export default class Record extends React.Component {
 			);
 		}
 		else {
-			return (
+
+			return (  //TODO Remove duplicated DOM insertion
 				<div>
 					<p>Share your groupSessionToken : {this.props.groupSessionToken}</p>
+					<HighchartsReact
+						highcharts={Highcharts}
+						options={{
+							chart: {
+								type: 'spline',
+								animation: Highcharts.svg,
+								height: 200
+							},
+							legend: {
+								enabled: false
+							},
+							title: null,
+							xAxis: {
+								type: 'datetime'
+							},
+							yAxis: {
+								title: null
+							},
+							series: [{
+								data: this.state.data
+							}]
+						}}
+					/>
 					<ButtonToolbar>
 						<Button bsStyle="primary" onClick={this.clickStart}>Record</Button>
 						<Button bsStyle="danger" onClick={this.clickLogout}>Logout</Button>
